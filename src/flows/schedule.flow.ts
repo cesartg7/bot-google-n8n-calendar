7008fefe226738441e20f5edc9e3f806127142c4
cesartg7 <cesartorregonzalez@gmail.com>
2024-08-22 18:45:15 +0200

    Arreglo pequeño error al recibir fecha de calendar n8n

--- src/flows/schedule.flow.ts
+++ src/flows/schedule.flow.ts
@@ -5,9 +5,9 @@ import { generateTimer } from "../utils/generateTimer";
 import { getCurrentCalendar } from "../services/calendar";
 import { getFullCurrentDate } from "src/utils/currentDate";
 import { flowConfirm } from "./confirm.flow";
-import { addMinutes, isWithinInterval, format, parse, isBefore  } from "date-fns";
+import { addMinutes, isWithinInterval, format, parse, isBefore } from "date-fns";
 
-const DURATION_MEET = process.env.DURATION_MEET ?? 55
+const DURATION_MEET = process.env.DURATION_MEET ?? 55;
 
 const PROMPT_FILTER_DATE = `
 ### Contexto
@@ -29,18 +29,18 @@ const generatePromptFilter = (history: string) => {
         .replace('{CURRENT_DAY}', nowDate);
 
     return mainPrompt;
-}
+};
 
 const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { extensions, state, flowDynamic, endFlow }) => {
     await flowDynamic('Dame un momento para consultar la agenda...');
     const ai = extensions.ai as AIClass;
     const history = getHistoryParse(state);
-    const list = await getCurrentCalendar()
+    const list = await getCurrentCalendar();
 
     const listParse = list
         .map(({ start, end }) => ({ fromDate: new Date(start), toDate: new Date(end) }));
 
-    console.log({ listParse })
+    console.log({ listParse });
 
     const promptFilter = generatePromptFilter(history);
 
@@ -51,7 +51,9 @@ const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { extensions,
         }
     ]);
 
+    // Convertimos la fecha del usuario a formato ISO 8601 (yyyy-MM-ddTHH:mm:ssZ)
     const desiredDate = parse(date, 'dd-MM-yyyy HH:mm:ss', new Date());
+    const desiredDateISO = format(desiredDate, "yyyy-MM-dd'T'HH:mm:ssXXX");
 
     // Verificar si la fecha es anterior a la fecha y hora actuales
     const now = new Date();
@@ -68,14 +70,14 @@ const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { extensions,
         const m = 'Lo siento, esa hora ya está reservada. ¿Alguna otra fecha y hora?';
         await flowDynamic(m);
         await handleHistory({ content: m, role: 'assistant' }, state);
-        return endFlow()
+        return endFlow();
     }
 
     const formattedDateFrom = format(desiredDate, 'hh:mm a');
     const formattedDateTo = format(addMinutes(desiredDate, +DURATION_MEET), 'hh:mm a');
     const message = `¡Perfecto! Tenemos disponibilidad de ${formattedDateFrom} a ${formattedDateTo} el día ${format(desiredDate, 'dd-MM-yyyy')}. ¿Confirmo tu reserva? *si*`;
     await handleHistory({ content: message, role: 'assistant' }, state);
-    await state.update({ desiredDate })
+    await state.update({ desiredDate: desiredDateISO }); // Guardar la fecha en formato ISO
 
     const chunks = message.split(/(?<!\d)\.\s+/g);
     for (const chunk of chunks) {
@@ -83,10 +85,10 @@ const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { extensions,
     }
 }).addAction({ capture: true }, async ({ body }, { gotoFlow, flowDynamic, state }) => {
 
-    if (body.toLowerCase().includes('si')) return gotoFlow(flowConfirm)
+    if (body.toLowerCase().includes('si')) return gotoFlow(flowConfirm);
 
-    await flowDynamic('¿Alguna otra fecha y hora?')
-    await state.update({ desiredDate: null })
-})
+    await flowDynamic('¿Alguna otra fecha y hora?');
+    await state.update({ desiredDate: null });
+});
 
-export { flowSchedule }
+export { flowSchedule };
