ccaf0715cdeef338061e351a025df13ce261679e
cesartg7 <cesartorregonzalez@gmail.com>
2024-08-23 23:36:33 +0200

    cambio de modelo GPT  a gpt-3.5-turbo-0301

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import flow from './flows';
 import { provider } from './provider';
 
 const PORT = process.env.PORT ?? 3001
-const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-3.5-turbo-0125')
+const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-3.5-turbo-0301')
 
 const main = async () => {
     const { httpServer, handleCtx } = await createBot({
